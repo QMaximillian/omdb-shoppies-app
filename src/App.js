33ac8944df22fd3fc4f8297018d38109c49896fa
“Quinn <quinnlashinsky@gmail.com>
2020-09-06 18:54:43 -0400

    Delete more unecessary code and add pagination
    Add ability to paginate between results of search term. Remove react logos from manifest.json and logo svg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import OMDBSearch from './components/OMDBSearch'
 const BASE_URL = `http://www.omdbapi.com/?`
+const RESULTS_PER_PAGE = 10
+
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
   const [results, setResults] = useState([])
+  const [page, setPage] = useState(1)
+
+
+  const handleMovieFetchCallback = useCallback(handleMovieFetch, [page, searchTerm])
+
+  React.useEffect(() => {
+    if (searchTerm) {
+      handleMovieFetchCallback(setResults)
+    }
+  }, [page, searchTerm, handleMovieFetchCallback])
 
   function handleSearch(event) {
     setSearchTerm(event.target.value)
   }
 
-  function handleMovieFetch(event, page = 1){
-    event.preventDefault()
+  
+  function handleMovieFetch(onFullfill, onError, options = {}){
     const data = {
       apiKey: process.env.REACT_APP_OMDB_KEY,
       s: searchTerm,
@@ -19,12 +31,7 @@ function App() {
     }
 
     const searchParams = new URLSearchParams(data).toString()
-    handleFetch(searchParams, setResults)
-
-  }
-
-  function handleFetch(searchParams, onFullfill, onError, url = BASE_URL, options = {}, ) {
-    fetch(`${url}${searchParams}`, {
+    return fetch(`${BASE_URL}${searchParams}`, {
       headers: {
       "Accept": "application/json",
     },
@@ -33,15 +40,29 @@ function App() {
     .then(response => response.json())
     .then(onFullfill)
     .catch(onError)
+
   }
 
+  function nextPage() {
+    setPage(currentPage => Math.ceil(results.totalResults / RESULTS_PER_PAGE) === currentPage ? currentPage : currentPage + 1)
+  }
+
+  function prevPage(){
+    setPage(currentPage => currentPage - 1 === 0 ? currentPage : currentPage - 1)
+  }
+
+
   return (
-    <form onSubmit={handleMovieFetch}>
+    <div>
+    <form>
       <OMDBSearch searchTerm={searchTerm} setSearchTerm={handleSearch} labelName={'omdb-search'} labelText={'Search'} />
       <pre>
         {JSON.stringify(results, undefined, 2)}
       </pre>
     </form>
+    <button onClick={prevPage}>{'<-'}</button>
+    <button onClick={nextPage}>{'->'}</button>
+    </div>
   );
 }
 
